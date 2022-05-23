f94d65f7e208e4db145ff0371c8e8194c736b70b
Ruby <55691511+okpc579@users.noreply.github.com>
2022-05-23 14:36:55 +0900

    first commit

---
+++ src/main/java/com/sideproject/spsgateway/SpsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.spsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsGatewayApplication.class, args);
+	}
+
+}
